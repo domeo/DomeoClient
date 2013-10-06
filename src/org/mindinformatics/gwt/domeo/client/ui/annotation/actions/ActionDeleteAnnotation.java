de7608cc171f73ac69752d8e9fec013882e7b735
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2013-10-06 15:13:57 -0400

    Added confirmation when deleting an annotation

--- src/org/mindinformatics/gwt/domeo/client/ui/annotation/actions/ActionDeleteAnnotation.java
+++ src/org/mindinformatics/gwt/domeo/client/ui/annotation/actions/ActionDeleteAnnotation.java
@@ -6,6 +6,7 @@ import org.mindinformatics.gwt.domeo.model.MAnnotation;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 
 public class ActionDeleteAnnotation {
 
@@ -13,9 +14,14 @@ public class ActionDeleteAnnotation {
 		ClickHandler ch = new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
-				domeo.getLogger().command(AnnotationFrameWrapper.LOG_CATEGORY_DELETE_ANNOTATION, clazz, "Item " + annotation.getClass().getName() + "-"+annotation.getLocalId());
-				// TODO manage deletion and undo???
-				domeo.getContentPanel().getAnnotationFrameWrapper().removeAnnotation(annotation, false);
+				boolean deleteFlag = Window.confirm("Do you really want to delete the annotation?");
+				if(deleteFlag) {
+					domeo.getProgressPanelContainer().setProgressMessage("Deleting annotation item...");
+					domeo.getLogger().command(AnnotationFrameWrapper.LOG_CATEGORY_DELETE_ANNOTATION, clazz, "Item " + annotation.getClass().getName() + "-"+annotation.getLocalId());
+					// TODO manage deletion and undo???
+					domeo.getContentPanel().getAnnotationFrameWrapper().removeAnnotation(annotation, false);
+					domeo.getProgressPanelContainer().setProgressMessage("Annotation deleted!");
+				}
 			}
 		};
 		return ch;
