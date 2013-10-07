7407db8694cd33904db55228716f1b536f27c104
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2013-10-06 21:15:23 -0400

    Fixed notification issue.

--- src/org/mindinformatics/gwt/domeo/client/ui/annotation/actions/ActionDeleteAnnotation.java
+++ src/org/mindinformatics/gwt/domeo/client/ui/annotation/actions/ActionDeleteAnnotation.java
@@ -20,7 +20,7 @@ public class ActionDeleteAnnotation {
 					domeo.getLogger().command(AnnotationFrameWrapper.LOG_CATEGORY_DELETE_ANNOTATION, clazz, "Item " + annotation.getClass().getName() + "-"+annotation.getLocalId());
 					// TODO manage deletion and undo???
 					domeo.getContentPanel().getAnnotationFrameWrapper().removeAnnotation(annotation, false);
-					domeo.getProgressPanelContainer().setProgressMessage("Annotation deleted!");
+					domeo.getProgressPanelContainer().setCompletionMessage("Annotation deleted!");
 				}
 			}
 		};
