fe8ad969f240c2f850b41a27995ccbc170cc4f38
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2013-10-09 12:17:41 -0400

    Cleanup

--- src/org/mindinformatics/gwt/domeo/plugins/annotation/comment/ui/tile/TCommentTile.java
+++ src/org/mindinformatics/gwt/domeo/plugins/annotation/comment/ui/tile/TCommentTile.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2013 Massachusetts General Hospital
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.mindinformatics.gwt.domeo.plugins.annotation.comment.ui.tile;
 
 import java.util.Date;
@@ -94,9 +114,6 @@ public class TCommentTile extends ATileComponent implements ITileComponent {
 		try {
 			createProvenanceBar(provenance, _annotation);
 			
-			/*
-			MAnnotationSet set = _domeo.getAnnotationPersistenceManager().getSetByAnnotationId(_annotation.getLocalId());
-*/
 			if(_annotation.getTitle()!=null && _annotation.getTitle().trim().length()>0) 
 				title.setText(_annotation.getTitle());
 			else 
