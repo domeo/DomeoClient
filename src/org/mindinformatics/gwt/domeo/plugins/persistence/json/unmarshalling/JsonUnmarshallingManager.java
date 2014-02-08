bbcabd7ec688dc2c040c85f712dd4870327442b1
rkboyce <rdb20@pitt.edu>
2014-02-08 08:51:38 -0500

    implemented suggestions from Paulo to address demarshalling issue

--- src/org/mindinformatics/gwt/domeo/plugins/persistence/json/unmarshalling/JsonUnmarshallingManager.java
+++ src/org/mindinformatics/gwt/domeo/plugins/persistence/json/unmarshalling/JsonUnmarshallingManager.java
@@ -668,23 +668,7 @@ public class JsonUnmarshallingManager {
 						((AnnotationPersistenceManager)_domeo.getPersistenceManager()).addAnnotation(ann, set);
 						set.setHasChanged(false);
 						
-					} else if (typesSet.contains(MSPLsAnnotation.TYPE)){
-						SPLType splType = null;
-						//if(typesSet.contains(SPLType.PHARMGX)) { //TODO: test with the conditional operational
-							splType = SPLType.PHARMGX_TYPE; //TODO: possibly add multiple types like Post It has (e.g., pharmgx, ddis, ades, etc)
-						//}
-						if(splType!=null) {
-							_domeo.getLogger().debug(this, "Calling unmarshallSPLAnnotation with selector 0 prefix: " + 
-									((MTextQuoteSelector)selectors.get(1)).getPrefix() + " suffix: " + 
-									((MTextQuoteSelector)selectors.get(1)).getSuffix());
-							ann = unmarshallSPLAnnotation((JsAnnotationSPL) jsonAnnotations.get(j), "", set, selectors);
-							((MSPLsAnnotation) ann).setType(splType);
-							
-							_domeo.getLogger().debug(this, "unmarshallSPLAnnotation produced annotation. Some tests values: getDrugOfInterest" + 
-									((MSPLsAnnotation) ann).getDrugOfInterest() + "getBiomarkers" + 
-									((MSPLsAnnotation) ann).getBiomarkers());							
-						}
-					} else {
+					}  else {
 						if(typesSet.contains(MPostItAnnotation.TYPE)) {
 							PostitType postItType = null;
 							if(typesSet.contains(PostitType.NOTE)) {
@@ -1098,6 +1082,22 @@ public class JsonUnmarshallingManager {
 									rr.setCreationDate(rel.getFormattedCreatedOn());
 									microPublication.getQualifiers().add(rr);
 								}
+							}
+						} else if (typesSet.contains(MSPLsAnnotation.TYPE)){
+							SPLType splType = null;
+							//if(typesSet.contains(SPLType.PHARMGX)) { //TODO: test with the conditional operational
+								splType = SPLType.PHARMGX_TYPE; //TODO: possibly add multiple types like Post It has (e.g., pharmgx, ddis, ades, etc)
+							//}
+							if(splType!=null) {
+								_domeo.getLogger().debug(this, "Calling unmarshallSPLAnnotation with selector 0 prefix: " + 
+										((MTextQuoteSelector)selectors.get(0)).getPrefix() + " suffix: " + 
+										((MTextQuoteSelector)selectors.get(0)).getSuffix());
+								ann = unmarshallSPLAnnotation((JsAnnotationSPL) jsonAnnotations.get(j), "", set, selectors);
+								((MSPLsAnnotation) ann).setType(splType);
+								
+								//_domeo.getLogger().debug(this, "unmarshallSPLAnnotation produced annotation. Some tests values: getDrugOfInterest" + 
+								//		((MSPLsAnnotation) ann).getDrugOfInterest() + "getBiomarkers" + 
+								//		((MSPLsAnnotation) ann).getBiomarkers());							
 							}
 						}
 						
